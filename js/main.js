9938956d3ad14f9a1c896ea13affdfbec36ed7b4
finbargiusti <finbargiusti@gmail.com>
2017-08-16 23:46:12 +0200

    Perfected navigation, addded more content

--- js/main.js
+++ js/main.js
@@ -17,3 +17,22 @@ function sleep(ms) {
 		header.innerHTML = (title.slice(0, i) + "|")
 	}
 })();
+
+window.onload = function() {
+	console.log(window.innerWidth);
+	if (window.innerWidth >= 1000) {
+		console.log(document.getElementById("doc-nav").style.height);
+		document.getElementById("doc-nav").style.height = document.getElementById("content-wrapper").clientHeight + "px";	
+	}	
+};
+
+window.onresize = function() {
+	if (window.innerWidth <= 1000) {
+		console.log(document.getElementById("doc-nav").style.height);
+		document.getElementById("doc-nav").style.height = "auto";	
+		document.getElementById("doc-nav").style.width = "100%"
+	} else {
+		document.getElementById("doc-nav").style.height = document.getElementById("content-wrapper").clientHeight + "px";	
+		document.getElementById("doc-nav").style.width = (window.innerWidth/5)+20 + "px";
+	}
+};
