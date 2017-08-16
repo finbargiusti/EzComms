f7e90f7ebfac4dffc8f24b20770ad47a52fd5a98
finbargiusti <finbargiusti@gmail.com>
2017-08-16 12:43:27 +0200

    Added title and content holder

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Declarations
+
+var header = document.querySelector("#header h1");
+var title = "EzComms";
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async function() {
+	var breakTime = 130;
+	await sleep(1000);
+	for (var i = 0; i <= title.length; ++i) {
+		await sleep(breakTime)
+		header.innerHTML = (title.slice(0, i) + "|")
+	}
+})();
